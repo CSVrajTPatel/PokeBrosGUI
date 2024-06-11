6e5caeec8425f543aff4971fa5a6beb7aa7f4d2f
shane wall <you@example.com>
2024-06-11 13:18:02 -0400

    adding open pack controller

---
+++ code/src/main/java/controllers/CardsInPackController.java
@@ -0,0 +1,19 @@
+package controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CardsInPackController {
+    @FXML
+    private Button switchMC;
+
+    @FXML
+    private Button switchP;
+
+    @FXML
+    private Button switchSC;
+
+    @FXML
+    private Button switchT;
+}
+
+
---
+++ code/src/main/java/controllers/SendTrade.java
@@ -0,0 +1,21 @@
+package controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class SendTrade {
+
+    @FXML
+    private Button BackToTrade;
+
+    @FXML
+    private TextField SearchCollection;
+
+    @FXML
+    private Button SendTrade;
+
+    @FXML
+    private TextArea TradeComment;
+
+}
