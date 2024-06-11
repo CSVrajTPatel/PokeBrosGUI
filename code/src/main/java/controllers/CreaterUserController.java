4d8d60c7156e26d46536ae91a85cafe9aab989b2
shane wall <you@example.com>
2024-06-11 00:26:47 -0400

    test

---
+++ code/src/main/java/controllers/CreaterUserController.java
@@ -0,0 +1,22 @@
+package controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CreaterUserController {    
+        @FXML
+        private TextField UsernameTxt;
+    
+        @FXML
+        private TextField UsernameTxt1;
+    
+        @FXML
+        private TextField UsernameTxt11;
+    
+        @FXML
+        private TextField UsernameTxt12;
+    
+        @FXML
+        private TextField UsernameTxt2;
+    
+    }
+    
---
+++ code/src/main/java/controllers/MyCollectionController.java
@@ -0,0 +1,20 @@
+package controllers;
+    import javafx.fxml.FXML;
+    import javafx.scene.control.Button;
+    
+    public class  MyCollectionController {
+    
+        @FXML
+        private Button MyCollection;
+    
+        @FXML
+        private Button Packs;
+    
+        @FXML
+        private Button SearchCards;
+    
+        @FXML
+        private Button Trade;
+    
+    }
+    
