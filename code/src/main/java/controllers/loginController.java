bc094c1ed6600a9db025d82a0c768b4d303d68f1
CSVrajTPatel <vpatel9073@outlook.com>
2024-06-10 20:13:54 -0400

    Added Login

--- code/src/main/java/controllers/loginController.java
+++ code/src/main/java/controllers/loginController.java
@@ -1,5 +1,10 @@
 package controllers;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 public class loginController {
-    
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField userName;
 }
