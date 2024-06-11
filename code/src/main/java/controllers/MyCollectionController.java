ff622c27cb2276e6d8827b7ed9fb02328d03efe6
CSVrajTPatel <vpatel9073@outlook.com>
2024-06-11 12:55:25 -0400

    Added pane to myCollection

--- code/src/main/java/controllers/MyCollectionController.java
+++ code/src/main/java/controllers/MyCollectionController.java
@@ -1,18 +1,24 @@
 package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
     
     public class  MyCollectionController {
 
-        @FXML
-        private Button switchP;
-    
-        @FXML
-        private Button switchSC;
-    
-        @FXML
-        private Button switchT;
-        
+    @FXML
+    private AnchorPane dispFav;
+
+    @FXML
+    private AnchorPane dispMyCollection;
+
+    @FXML
+    private Button switchP;
+
+    @FXML
+    private Button switchSC;
+
+    @FXML
+    private Button switchT;
     
     }
     
